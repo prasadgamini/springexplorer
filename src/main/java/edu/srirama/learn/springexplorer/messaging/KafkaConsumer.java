aa94d4634c7700521d384086708ee89c996570a6
Prasad Gamini <prasadgamini@SriRama-MBP-14.attlocal.net>
2024-12-20 22:20:59 -0600

    KafkaConsumer will save the student record in Postgres DB

--- src/main/java/edu/srirama/learn/springexplorer/messaging/KafkaConsumer.java
+++ src/main/java/edu/srirama/learn/springexplorer/messaging/KafkaConsumer.java
@@ -1,8 +1,12 @@
 package edu.srirama.learn.springexplorer.messaging;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.srirama.learn.springexplorer.entity.Student;
+import edu.srirama.learn.springexplorer.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -11,10 +15,16 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class KafkaConsumer {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private StudentService studentService;
 
     @KafkaListener(topics = "${custom.consumer-topic-name}", groupId = "${custom.consumer-group-id}")
     public void consumeMessages(ConsumerRecord<?, Object> event, Acknowledgment acknowledgment) {
-        log.info(".KafkaConsumer.consumeMessages. Message Received:{}", event.value());
+        Student student = objectMapper.convertValue(event.value(), Student.class);
+        log.info(".KafkaConsumer.consumeMessages. Message Received:{}", student);
+        studentService.saveStudent(student);
         acknowledgment.acknowledge();
     }
 }
